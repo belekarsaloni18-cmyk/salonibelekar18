6950cf76d867ce1195ba7218d16fb595ec7491ca
belekarsaloni18-cmyk <belekarsaloni18@gmail.com>
2026-02-18 12:14:25 +0530

    Add files via upload

---
+++ SalonBelekar.5.py
@@ -0,0 +1,14 @@
+<html>
+      <body>
+   <header>
+<h2>SBJIT nagpur</h2>
+</header>
+<section>
+<artical>
+<h2>Understanding HTML5</h2>
+<p>HTML5 introduction semantic element to improve structure.</p>
+</artical>
+<artical>
+<h2>Wht use Semantic Tags?</h2>
+<p>They improve accessibility and SEO.</p>
+</article>
